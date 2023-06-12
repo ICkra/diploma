da5a7885c55e16abde3e151b762afaa2b46ba1af
Iryna Chernyukh <god.windows3@gmail.com>
2023-06-11 22:50:42 -0700

    add gitignore to gitignore

--- src/config.php
+++ src/config.php
@@ -1,55 +1,55 @@
 <?php
 
-defined(‘IYRYST’) or die(‘Access denied’);
+defined('IYRYST') or die('Access denied');
 
 // домен
-define(‘PATH’, ‘http://www.vash-dogovir.com.ua//’);
+define('PATH', 'http://www.vash-dogovir.com.ua//');
 
 // модель
-define(‘MODEL’, ‘model/model.php’);
+define('MODEL', 'model/model.php');
 
 // контролер
-define(‘CONTROLLER’, ‘controller/controller.php’);
+define('CONTROLLER', 'controller/controller.php');
 
 // вид
-define(‘VIEW’, ‘views/’);
+define('VIEW', 'views/');
 
 // папка з активним шаблоном
-define(‘TEMPLATE’, PATH.VIEW.’ivystavka/’);
+define('TEMPLATE', PATH.VIEW.'ivystavka/');
 
 // папка з картинками контента
-define(‘DOCIMG’, PATH.’userfiles/doc_img/baseimg/’);
+define('DOCIMG', PATH.'userfiles/doc_img/baseimg/');
 
 // папка з картинками галереї
-define(‘GALLERYIMG’, PATH.’userfiles/doc_img/’);
+define('GALLERYIMG', PATH.'userfiles/doc_img/');
 
 // максимально допустима вага завантажуваних картинок - 1 Мб
-define(‘SIZE’, 1048576);
+define('SIZE', 1048576);
 
 // сервер БД
-define(‘HOST’, ‘localhost’);
+define('HOST', 'localhost');
 
 // користувач
-define(‘USER’, ‘root’);
+define('USER', 'root');
 
 // пароль
-define(‘PASS’, ‘‘);
+define('PASS', '');
 
 // БД
-define(‘DB’, ‘doc’);
+define('DB', 'doc');
 
 // назва - title
-define(‘TITLE’, ‘Система виставок’);
+define('TITLE', 'Система виставок');
 
 // email адміністратора
-define(‘ADMIN_EMAIL’, ‘admin@doc.com’);
+define('ADMIN_EMAIL', 'admin@doc.com');
 
 // кількість на сторінці
-define(‘PERPAGE’, 9);
+define('PERPAGE', 9);
 
 // папка шаблонов адміністративної частини
-define(‘ADMIN_TEMPLATE’, ‘templates/’);
+define('ADMIN_TEMPLATE', 'templates/');
 
-mysql_connect(HOST, USER, PASS) or die(‘No connect to Server’);
-mysql_select_db(DB) or die(‘No connect to DB’);
-mysql_query("SET NAMES ‘UTF8’") or die(‘Cant set charset’);
+mysql_connect(HOST, USER, PASS) or die('No connect to Server');
+mysql_select_db(DB) or die('No connect to DB');
+mysql_query("SET NAMES 'UTF8'") or die('Cant set charset');
